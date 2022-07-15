60359a49777914e02945ea6a82521821bec2e1f8
ChenReuven <chen.rvn@gmail.com>
2022-07-15 06:49:53 +0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Chen</h1>
+      <h1>Vite + React</h1>
+      <h5>Chen Example</h5>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
